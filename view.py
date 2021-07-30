52845bf13b431403a4fe7aad77d7022a92bde61d
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2021-07-30 20:37:05 +0800

    Separated from handler.py

---
+++ view.py
@@ -0,0 +1,31 @@
+from queue import Queue
+from asyncio import get_running_loop
+
+
+class View:
+    def __init__(self, func, take_request=False):
+        self.func = func
+        self.take_request = take_request
+        self.await_send = False
+        self.request = None
+
+    def set_request(self, request):
+        self.request = request
+
+    def set_await_send(self, await_send):
+        self.await_send = await_send
+
+    def __call__(self, *args, **kwargs) -> Queue:
+        result = Queue()
+        loop = get_running_loop()
+
+        async def send(resp):
+            async def inner(): return resp
+            result.put(loop.create_task(inner()))
+            return resp
+
+        if self.take_request: args = (self.request,) + args
+        if self.await_send: args = (send,) + args
+
+        result.put(loop.create_task(self.func(*args, **kwargs)))
+        return result
