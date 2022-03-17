73fa02a69c70f6561c5e9336cb19daa728d5b2fc
VincentChui2005 <51741497+VincentChui2005@users.noreply.github.com>
2022-03-17 23:34:10 +0800

    rename response.Response.extra_work() to response.Response._extra_work() to avoid confusion  # it is not supposed to be used split response.Redirect._extra_work() to have url-to-redirect and the execution code to be extractable via response.Redirect.as_html()  # nesting a redirection inside another Redirect response is possible

--- kripweb/response.py
+++ kripweb/response.py
@@ -36,7 +36,7 @@ class Response:
 
     def set_handler(self, handler):
         self.handler = handler
-        self.extra_work()
+        self._extra_work()
 
     def set_cookie(self, key, value):
         self.cookies[key] = value
@@ -46,7 +46,7 @@ class Response:
         self.callback_be_awaited = be_awaited
         return self
 
-    def extra_work(self):
+    def _extra_work(self):
         pass
 
 
@@ -68,7 +68,7 @@ class FileResponse(Response):
     def __init__(self, path, filename, as_attachemnt=False):
         super().__init__()
         self.content_type = "application/octet-stream"
-        self.headers[b"content-disposition"] = f"{'attachment' if as_attachemnt else 'inline'}; filename='{filename}'".encode()
+        self.headers[b"content-disposition"] = f"{'attachment' if as_attachemnt else 'inline'}; filename={filename}".encode()
         with open(path, "rb") as f:
             self.body_content = f.read()
 
@@ -76,17 +76,10 @@ class FileResponse(Response):
 class StaticResponse(Response):
     def __init__(self, path):
         super().__init__()
-        match path.split(".")[-1]:
-            case "js":
-                self.content_type = "text/javascript"
-            case "css":
-                self.content_type = "text/css"
-            case _:
-                self.content_type = "application/octet-stream"
-            
+        self.content_type = "application/octet-stream"
         self.path = path
 
-    def extra_work(self):
+    def _extra_work(self):
         self.headers[b"content-disposition"] = b"inline"
         try:
             with open(path := (self.handler.setting.static_path + self.path), "rb") as f:
@@ -110,13 +103,13 @@ class HTMLResponse(Response):
         self.variables = variables
         return self
 
-    def extra_work(self):
+    def _extra_work(self):
         if self.path:
             self.body_content = self.handler.setting.jinja2_env.get_template(self.path).render(**self.variables).encode()
 
 
 class Redirect(Response):
-    def __init__(self, url="", on_redir_page="", redir_delay=0):
+    def __init__(self, url="", on_redir_page="", redir_delay=0, new_tab=False):
         super().__init__()
         self.content_type = "text/html"
         self.status_code = 302
@@ -124,26 +117,30 @@ class Redirect(Response):
         self.url = url
         self.page = on_redir_page
         self.delay = redir_delay
+        self.new_tab = new_tab
 
         self.page_name = None
         self.from_subpages = None
         self.url_suffix = ""
 
     @classmethod
-    def to_view(cls, page_name, from_subpages="", url_suffix="", on_redir_page="", redir_delay=0):
-        self = cls(on_redir_page=on_redir_page, redir_delay=redir_delay)
+    def to_view(cls, page_name, from_subpages="", url_suffix="", on_redir_page="", redir_delay=0, new_tab=False):
+        self = cls(on_redir_page=on_redir_page, redir_delay=redir_delay, new_tab=new_tab)
         self.page_name = page_name
         self.from_subpages = from_subpages
         self.url_suffix = url_suffix
         return self
 
-    def extra_work(self):
-        url = (self.url if self.page_name is None else self.handler.name_to_url(self.page_name, self.from_subpages)) + self.url_suffix
-        self.status = f"Redirected to {url}"
-        self.body_content = f"""
+    def as_html(self, handler):
+        self.url = (self.url if self.page_name is None else handler.name_to_url(self.page_name, self.from_subpages)) + self.url_suffix
+        return f"""
             {self.page}
-            <script>setTimeout(function(){'{'}window.location.replace('{url}'){'}'}, {self.delay})</script>
-        """.encode()
+            <script>setTimeout(function(){'{'}window.open('{self.url}', '{'_blank' if self.new_tab else '_parent'}'){'}'}, {self.delay})</script>
+        """
+
+    def _extra_work(self):
+        self.status = f"Redirected to {self.url}"
+        self.body_content = self.as_html(self.handler).encode()
 
 
 def errorize(resp: Response, error_code: (int or str)) -> Response:
